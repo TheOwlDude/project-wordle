d6f8dc94a5888119bd47ed6584f4d8bd9d238fbc
Brian <brian.cole@opensesame.com>
2025-01-01 10:43:10 -0800

    add cheatmode component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import Game from '../Game';
 import Header from '../Header';
 import {createContext} from "react";
 import GameStateProvider from "../GameStateProvider";
-import ConsistentWords from "../ConsistentWords";
+import CheatMode from "../CheatMode";
 
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
 
         <div className="game-wrapper">
           <Game />
-          <ConsistentWords />
+          <CheatMode />
         </div>
 
       </div>
